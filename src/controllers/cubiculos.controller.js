fcc24772bd5f6c177efde38c95b0c04646cae2be
maurisq12 <maurisq@hotmail.com>
2023-06-04 17:54:45 -0600

    base solo con cubiculos y estudiantes

--- src/controllers/cubiculos.controller.js
+++ src/controllers/cubiculos.controller.js
@@ -4,5 +4,4 @@ export const getCubiculos = async (req, res) => {
     const con = await getConnection();
     const resp= await con.request().query("SELECT idCubiculo, nombre, capacidad, EstadosCubiculo.estadoActual estado, tiempoMaximo FROM Cubiculos INNER JOIN EstadosCubiculo ON Cubiculos.idEstado = EstadosCubiculo.idEstado");
     res.json(resp.recordset)
-
 }
